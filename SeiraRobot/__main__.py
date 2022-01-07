7b843cef47258d635e4a86eb1e1926ed80e61444
AttitudefuckerXD <95916276+AttitudefuckerXD@users.noreply.github.com>
2022-01-07 15:18:08 +0530

    Update __main__.py

--- SeiraRobot/__main__.py
+++ SeiraRobot/__main__.py
@@ -6,12 +6,12 @@ import time
 import re
 import sys
 import traceback
-import EmikoRobot.modules.sql.users_sql as sql
+import SeiraRobot.modules.sql.users_sql as sql
 from sys import argv
 from typing import Optional
 from telegram import __version__ as peler
 from platform import python_version as memek
-from EmikoRobot import (
+from SeiraRobot import (
     ALLOW_EXCL,
     CERT_PATH,
     DONATION_LINK,
